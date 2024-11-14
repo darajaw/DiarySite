2c1701d2eaea44f1a11e19ca2a14361338d22aa4
Daraja Williams <will1158@algonquinlive.com>
2024-11-14 12:54:11 -0500

    inital php added

---
+++ search.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<!--Student Names: Daraja Williams & Stephanie Prystupa-Maule-->
+<!--File Name:entry.html-->
+<!--Date Created: November 14, 2024-->
+<!--Description: This page is used to search and filter through previous journal entries.-->
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Daraja Williams & Stephanie Prystupa-Maule">        
+    <meta name="description" content="This page is used to search and filter through previous journal entries.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    include_once("header.php"); 
+
+    include_once("footer.php") ;
+
+</body>
+</html>

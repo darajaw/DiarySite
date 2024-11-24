a0b95cc91642214d62c0483c43600394beba544a
Stephanie Prystupa-Maule <prys0009@algonquinlive.com>
2024-11-24 12:07:40 -0500

    Matched structure between header and footer files

--- footer.php
+++ footer.php
@@ -1,12 +1,16 @@
+<!--
+TODO: 
+-add submission file notes, e.g. authorship purpose
+--> 
+
 <!DOCTYPE html>
 <html lang="en">
 
 </html>
-<!-- footer for all pages-->
-        <footer><hr>
-            <p id="foot_text">&copy; 2024 Daraja Williams & Stephanie Prystupa-Maule</p>
-        </footer>
-
-    </body>
-
+<body>
+    <!-- footer for all pages-->
+    <footer>
+        <p id="foot_text">&copy; 2024 Daraja Williams & Stephanie Prystupa-Maule</p>
+    </footer>
+</body>
 </html>
--- header.php
+++ header.php
@@ -1,19 +1,15 @@
+<!-- 
+ TODO
+-add submission file notes, e.g. authorship purpose
+ -->
+
 <!DOCTYPE html>
 
 <html lang="en">
 
-<head>
-  <!-- header for all pages, except homepage-->
-  <meta charset="UTF-8">
-  <meta name="author" content="Daraja Williams & Stephanie Prystupa-Maule">  
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets/style.css">
-</head>
-
 <body>
-  <header>
-      <div id="title-container">
-        <h1 id="title">Smart Diary</h1>
-      </div>
+  <!-- footer for all pages-->
+  <header> 
+    <h1 id="title">Smart Diary</h1>
   </header>
 </body>
